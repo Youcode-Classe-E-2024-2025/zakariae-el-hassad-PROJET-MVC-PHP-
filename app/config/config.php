596a6506b778393e9053236608b6ec7fa0322a32
zakariaeelhassad <zakariaelhassad031@gmail.com>
2025-02-06 11:45:37 +0100

    Ajout de la page config dans le dossier config

---
+++ app/config/config.php
@@ -0,0 +1,15 @@
+<?php 
+
+return [
+    'database' => [
+        'host' => 'localhost',
+        'port' => '5432',
+        'dbname' => 'nom_de_ta_base',
+        'user' => 'utilisateur',
+        'password' => 'mot_de_passe',
+    ],
+        'app' => [
+            'base_url' => '/zakariae-el-hassad-PROJET-MVC-PHP-/public'  // Chouf dirti /public
+        ]
+];
+
